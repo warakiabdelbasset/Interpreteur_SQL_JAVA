722a521d511036c018a8328dcf72b68731af92e0
abdelbasset <abdelbassetwaraki@gmail.com>
2021-07-31 13:54:07 +0100

    f

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 public class Main {
-    // String from = words[3
-    // for (String word : words){
-    // list.getid().equals(1)
-    // }
-
-    // Scanner sc = new Scanner(System.in); // System.in is a standard input stream
 
     public static void main(String[] args) {
 
@@ -16,25 +10,71 @@ public class Main {
         System.out.print("Enter a string: ");
         String a = sc.nextLine(); // reads string
         String[] words = a.split(" ");
-
+        p.add(new Person("1", "mohamed", "61"));
+        p.add(new Person("3", "ddde", "485"));
+        p.add(new Person("2", "casillas", "44"));
         if (words[0].equals("insert") && words[1].equals("into") && words[2].equals("Person") && words[3].equals("(")
                 && words[4].equals("id") && words[5].equals(",") && words[6].equals("name") && words[7].equals(",")
                 && words[8].equals("age") && words[9].equals(")") && words[10].equals("values") && words[11].equals("(")
                 && words[13].equals(",") && words[15].equals(",") && words[17].equals(")")) {
 
-            p.add(new Person("1", "mohamed", "61"));
-            p.add(new Person("2", "casillas", "44"));
             p.add(new Person(words[12], words[14], words[16]));
             System.out.println(p.size());
             for (Person e : p)
                 e.AfficherValeur();
-                System.out.println("DONE.....");
-        } else {
-            System.out.println("please enter the correct syntax");
+            System.out.println("DONE.....");
+        } else if (words[0].equals("select") && words[1].equals("*") && words[2].equals("from")
+                && words[3].equals("Person") && words[4].equals("where") && words[5].equals("id")
+                ) {
+                    if(words[6].equals("=")){
+                        for (Person e : p) {
+
+                            if (e.getId().equals(words[7])) {
+                                e.AfficherValeur();
+                            }
+            
+                        }
+                    }else if(words[6].equals(">")){
+                        
+                        for (Person e : p) {
+                            int id = Integer.parseInt(words[7]);
+                            int idE = Integer.parseInt(e.getId());
+                            if (idE > id) {
+                                e.AfficherValeur();
+                            }
+            
+                        }
+                    }
+            
 
         }
 
-        // System.out.println(s.a());
+        else if (words[0].equals("update") && words[1].equals("Person") && words[2].equals("set")
+                && words[3].equals("name") && words[4].equals("=") && words[6].equals(",") && words[7].equals("age")
+                && words[8].equals("=") && words[10].equals("where") && words[11].equals("id")
+                && words[12].equals("=")) {
 
+            for (Person e : p) {
+
+                if (e.getId().equals(words[13])) {
+                    e.setName(words[5]);
+                    e.setAge(words[9]);
+                }
+                e.AfficherValeur();
+
+            }
+
+        } else if (words[0].equals("delete") && words[1].equals("from") && words[3].equals("where")
+                && words[4].equals("id") && words[5].equals("=")) {
+
+            for (Person e : p) {
+
+                if (e.getId().equals(words[6])) {
+                    p.remove(e);
+
+                }
+                e.AfficherValeur();
+            }
+        }
     }
 }
