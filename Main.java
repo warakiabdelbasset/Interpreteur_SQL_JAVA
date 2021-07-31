66e19fc909a38348659ff76c5e05c673c845e8ae
abdelbasset <abdelbassetwaraki@gmail.com>
2021-07-31 21:52:47 +0100

    s

--- Main.java
+++ Main.java
@@ -13,42 +13,75 @@ public class Main {
         p.add(new Person("1", "mohamed", "61"));
         p.add(new Person("3", "ddde", "485"));
         p.add(new Person("2", "casillas", "44"));
+        //////////////////// insert //////////////////////////////
         if (words[0].equals("insert") && words[1].equals("into") && words[2].equals("Person") && words[3].equals("(")
                 && words[4].equals("id") && words[5].equals(",") && words[6].equals("name") && words[7].equals(",")
                 && words[8].equals("age") && words[9].equals(")") && words[10].equals("values") && words[11].equals("(")
                 && words[13].equals(",") && words[15].equals(",") && words[17].equals(")")) {
 
             p.add(new Person(words[12], words[14], words[16]));
-            System.out.println(p.size());
             for (Person e : p)
                 e.AfficherValeur();
             System.out.println("DONE.....");
-        } else if (words[0].equals("select") && words[1].equals("*") && words[2].equals("from")
-                && words[3].equals("Person") && words[4].equals("where") && words[5].equals("id")
+        } 
+        ////////////////// select ///////////////////////////////
+        else if (words[0].equals("select") && words[1].equals("*") && words[2].equals("from")
+                && words[3].equals("Person") && words[4].equals("where")
                 ) {
+                if(words[5].equals("id")){
                     if(words[6].equals("=")){
                         for (Person e : p) {
-
                             if (e.getId().equals(words[7])) {
                                 e.AfficherValeur();
-                            }
-            
+                            }      
                         }
-                    }else if(words[6].equals(">")){
-                        
+                        System.out.println("DONE.....");    
+                    }else if(words[6].equals(">")){                       
                         for (Person e : p) {
                             int id = Integer.parseInt(words[7]);
                             int idE = Integer.parseInt(e.getId());
                             if (idE > id) {
                                 e.AfficherValeur();
-                            }
-            
+                            }           
                         }
+                        System.out.println("DONE....."); 
+                    }else if(words[6].equals("<")){                        
+                        for (Person e : p) {
+                            int id = Integer.parseInt(words[7]);
+                            int idE = Integer.parseInt(e.getId());
+                            if (idE < id) {
+                                e.AfficherValeur();
+                            }   
+                        }
+                        System.out.println("DONE.....");        
                     }
-            
+                }
+                else if(words[5].equals("name")){
+                    if(words[6].equals("=")){
+                        for (Person e : p) {
+                            if (e.getName().equals(words[7])) {
+                                e.AfficherValeur();
+                            }                                    
+                        }
+                        System.out.println("DONE....."); 
+                    }
+                }
+                else if(words[5].equals("age")){
+                    if(words[6].equals("=")){
+                        for (Person e : p) {
+                            if (e.getAge().equals(words[7])) {
+                                e.AfficherValeur();
+                            }       
+                        }
+                        System.out.println("DONE.....");   
+                    }
+                }
+                else{
+                    System.out.println("Please enter a valid request....");
+                }
 
         }
-
+        /////////////////// update //////////////////////////
         else if (words[0].equals("update") && words[1].equals("Person") && words[2].equals("set")
                 && words[3].equals("name") && words[4].equals("=") && words[6].equals(",") && words[7].equals("age")
                 && words[8].equals("=") && words[10].equals("where") && words[11].equals("id")
@@ -64,7 +97,9 @@ public class Main {
 
             }
 
-        } else if (words[0].equals("delete") && words[1].equals("from") && words[3].equals("where")
+        } 
+        ///////////////////// delete /////////////////////
+        else if (words[0].equals("delete") && words[1].equals("from") && words[3].equals("where")
                 && words[4].equals("id") && words[5].equals("=")) {
 
             for (Person e : p) {
